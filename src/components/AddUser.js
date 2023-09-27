09778793a3c7db9882aeec67a00e95d3cd8bfcb4
Mohammad Shams Tabrez <shamstabrez.raw@gmail.com>
2023-09-27 11:27:36 -0500

    useRef hook added
    useRef hook added

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,61 +1,68 @@
-import React, { useState } from "react";
+import React, { useState,useRef } from "react";
 import classes from "./AddUser.module.css";
 import Card from "./UI/Card";
 import Button from "./UI/Button";
 import ErrorModal from "./UI/ErrorModal";
 
 const AddUser = (props) => {
-  const [userName, setUserName] = useState("");
-  const [userAge, setUserAge] = useState("");
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
   const [error, setError] = useState("");
-  const userNameChangeHandler= event =>{
-    setUserName(event.target.value);
-  }
-  const userAgeChangeHandler= event =>{
-    setUserAge(event.target.value);
-  }
-
   const submitHandler = (event) => {
+    const entertedUserName =nameInputRef.current.value;
+    const entertedUserAge =ageInputRef.current.value;
     event.preventDefault();
-    if(userName.trim().length===0 || userAge.trim().length ===0 ){
-        setError({
-            title: 'Invalid input',
-            message: 'Please enter a valid name and age (non-empty values)'
-        })
-        return ;
+    if (entertedUserName.trim().length === 0 || entertedUserAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values)",
+      });
+      return;
+    }
+    if (+entertedUserAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age  greater than 0",
+      });
+      return;
+    }
 
-    }
-    if(+userAge<1){
-        setError({
-            title: 'Invalid age',
-            message: 'Please enter a valid age  greater than 0'
-        })
-        return;
-    }
-    
-    props.onAddUser(userName,userAge);
-    setUserAge("");
-    setUserName('');
+    props.onAddUser(entertedUserName, entertedUserAge);
+    nameInputRef.current.value='';
+    ageInputRef.current.value=" ";
   };
 
-  const ErrorHandler =()=>{
+  const ErrorHandler = () => {
     setError(null);
-  }
+  };
 
   return (
-    <div>
-    {error && <ErrorModal  onConfirm={ErrorHandler} title={error.title} message ={error.message}></ErrorModal>}
-    <Card className={classes.input}>
-      <form onSubmit={submitHandler}>
-        <label htmlFor="addname">User name</label>
-        <input value={userName} onChange={userNameChangeHandler} id="addname" type="text" />
-        <label htmlFor="age">Age (Years) </label>
-        <input value={userAge}  onChange={userAgeChangeHandler} id="age" type="number" />
-        <Button type="submit">Add user </Button>
-      </form>
-    </Card>
-          
-    </div>
+    <>
+      {error && (
+        <ErrorModal
+          onConfirm={ErrorHandler}
+          title={error.title}
+          message={error.message}
+        ></ErrorModal>
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={submitHandler}>
+          <label htmlFor="addname">User name</label>
+          <input
+            id="addname"
+            type="text"
+            ref ={nameInputRef}
+          />
+          <label htmlFor="age">Age (Years) </label>
+          <input
+            id="age"
+            type="number"
+            ref={ageInputRef}
+          />
+          <Button type="submit">Add user </Button>
+        </form>
+      </Card>
+    </>
   );
 };
 
--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,15 +1,16 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
 
-const ErrorModal = (props) => {
+const BackDrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onConfirm}></div>;
+};
+const ModalOverLay = (props) => {
   return (
-    <div>
-    <div className={classes.backdrop} onClick={props.onConfirm}></div>
-
-    <Card className ={classes.modal}>
+    <Card className={classes.modal}>
       <header className={classes.header}>
         <h2>{props.title}</h2>
       </header>
@@ -17,11 +18,28 @@ const ErrorModal = (props) => {
         <p>{props.message}</p>
       </div>
       <footer className={classes.actions}>
-        <Button  onClick={props.onConfirm}> Okay</Button>
+        <Button onClick={props.onConfirm}> Okay</Button>
       </footer>
     </Card>
-            
-    </div>
+  );
+};
+const ErrorModal = (props) => {
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <BackDrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverLay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
+      ;
+    </React.Fragment>
   );
 };
 export default ErrorModal;
